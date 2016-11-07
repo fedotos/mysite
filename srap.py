0c721d6969715de3cd26ecd5cda74ce2bdd26967
vlad <nivalsss@gmail.com>
2016-11-07 22:38:35 +0200

    07-11-2016 home

--- srap.py
+++ srap.py
@@ -3,13 +3,19 @@
 __author__ = 'fdts'
 
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+from urllib.parse import urlencode
 
 SITE1 = "http://sharik.cv.ua/index.php"
 SITE2 = "http://freebookspot.es/"
 SITE3 = "http://sharik.cv.ua/page1.php"
 
-html = urlopen(SITE1)
+try:
+    html = urlopen(SITE1)
+except HTTPError as e:
+    print(e)
+    exit()
 bsobj = BeautifulSoup(html.read(),"html.parser")
 print(html.geturl()) #из книги дронов
 print(html.info())
@@ -24,12 +30,14 @@ print(bsobj.head)
 print(bsobj.div)
 print(bsobj.title)
 
-try:
-    html1 = urlopen(SITE3)
-except HTTPError as e:
-    print(e)
-    exit()
-bsobj = BeautifulSoup(html1.read(),"html.parser")
-print(bsobj.head)
-
-
+encoding='utf-8'
+url = 'https://www.kinopoisk.ru/user/2007910/votes/list/ord/year/#list' # url для второй страницы
+r = urlopen(url)
+print("*************************")
+print(r.info())
+print("*************************")
+bsobj1 = BeautifulSoup(r.read(), "html.parser")
+print(bsobj1.head)
+print(bsobj1.body)
+# with open('test.html', 'w') as output_file:
+#  output_file.write(r.text.encode('cp1251'))
